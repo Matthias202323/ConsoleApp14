701220da7737fac1683e83ed7a31926e03fbc726
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-04-30 05:43:55 +0200

    commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Amount ?");
+           
+            double amount = Convert.ToDouble(Console.ReadLine());
+            ShowMoney currency = Euros;
+            currency(amount);
+            currency = Dollars;
+            currency(amount);
+        }
+
+        public delegate void ShowMoney(double amount);
+
+        public static void Euros(double amount)
+        {
+            Console.WriteLine(amount + "€");
+        }
+
+        public static void Dollars(double amount)
+        {
+            Console.WriteLine(amount + "$");
+        }
+    }
+}
